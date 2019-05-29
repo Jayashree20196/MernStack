88800cb2e1652152e9bbf384d674ae6fef0aec3e
Jayashree20196 <njayashree20196@gmail.com>
2019-05-29 18:00:00 +0530

    Added client side script(UI) using react.

---
+++ Mern-step1/client/src/Components/Navbar.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+
+    state = {
+        isOpen: false
+    };
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark expand="sm" className="mb-5">
+                    <Container>
+                        <NavbarBrand href="/">TestApp</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle}></NavbarToggler>
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" Navbar>
+                                <NavItem>
+                                    <NavLink href="/components/">Jayashree</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default AppNavbar;
